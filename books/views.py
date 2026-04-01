55e166a6130dfae4dea707fc65ef6dfa3a2e0607
Islam <islam.qiyasov0823@gmail.com>
2026-04-02 01:59:18 +0500

    Add Pillow dependency

--- books/views.py
+++ books/views.py
@@ -5,7 +5,7 @@ from .serializers import AuthorSerializer, BookSerializer, GenreSerializer, Publ
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset=Author.objects.all()
