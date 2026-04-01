ece72c3ff4a746f706807786ff78258070bcfac0
Islam <islam.qiyasov0823@gmail.com>
2026-04-02 03:27:42 +0500

    Add Pillow dependency

--- library/settings.py
+++ library/settings.py
@@ -108,10 +108,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
 
 
