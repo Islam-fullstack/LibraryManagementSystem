5bdc7765df6ce26879a494c3543cc289dcedc6e9
Islam <84979162+Islam-fullstack@users.noreply.github.com>
2026-05-22 22:31:13 +0500

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -62,12 +62,11 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
